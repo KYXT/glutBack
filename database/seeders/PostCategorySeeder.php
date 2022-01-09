af4c698d5d5e2202b6a23c0b81f5f0db84dce8d5
KYXT <pazayats@gmail.com>
2022-01-09 23:55:47 +0100

    seed fix

--- database/seeders/PostCategorySeeder.php
+++ database/seeders/PostCategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PostCategory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,43 +15,39 @@ class PostCategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('post_categories')->insert([
+        $data = [
             [
-                'id'        => 1,
                 'lang'      => 'pl',
                 'slug'      => 'aktualnosci',
                 'name'      => 'Aktualności',
             ],
             [
-                'id'        => 2,
                 'lang'      => 'pl',
                 'slug'      => 'przepisy-kulinarne',
                 'name'      => 'Przepisy kulinarne',
             ],
             [
-                'id'        => 3,
                 'lang'      => 'pl',
                 'slug'      => 'dokumenty',
                 'name'      => 'Dokumenty',
             ],
             [
-                'id'        => 4,
                 'lang'      => 'ru',
                 'slug'      => 'novosti',
                 'name'      => 'Новости',
             ],
             [
-                'id'        => 5,
                 'lang'      => 'ru',
                 'slug'      => 'recepti',
                 'name'      => 'Рецепты',
             ],
             [
-                'id'        => 6,
                 'lang'      => 'ru',
                 'slug'      => 'dokumenti',
                 'name'      => 'Документы',
             ],
-        ]);
+        ];
+
+        PostCategory::insert($data);
     }
 }
